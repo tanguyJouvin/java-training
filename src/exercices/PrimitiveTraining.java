79509c997f350f520d29b4b5d79e565ecaf14bf7
tanguy jouvin <tanguyJouvin@protonmail.com>
2020-03-19 19:37:32 +0100

    finish PrimitiveTraining

--- src/exercices/PrimitiveTraining.java
+++ src/exercices/PrimitiveTraining.java
@@ -2,17 +2,32 @@ public class PrimitiveTraining {
 
     // TODO 1 : Change the method 'integer' to return the expected result : int 42
     public static int integer() {
-        return 0;
+        int number = 42 ;
+        return number;
     }
 
     // TODO 2 : Create a method 'str', result expected : String "Hello"
 
-
+    public static String str() {
+        final String hello = "Hello";
+        return hello;
+    }
     // TODO 3 : Create a method 'real', result expected : double 3.14
 
-
+    public static double real() {
+        final double doubleNum = 3.14;
+        return doubleNum;
+    }
     // TODO 4 : Create a method 'bool', result expected : boolean false
 
+    public static boolean bool() {
+        boolean boolFalse = false;
+        return boolFalse;
+    }
 
     // TODO 5 : Create a method 'nothing', no result expected
+    public static String nothing() {
+        String s = null;
+        return s;
+    }
 }
